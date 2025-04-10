6473517ba99e74b6d133c4b02c38f6961f8b61a9
Tatiana Haveroth Barbosa <haverothtatiana@gmail.com>
2025-04-10 09:05:56 -0300

    primeiro commit para adicionar o projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MeuProjetoMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
